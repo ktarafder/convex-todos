84dfe4c827cb7c66df364fce65d8fe535ccc01f6
ktarafder <ktarafder01@gmail.com>
2024-09-15 22:25:43 -0400

    Remove unused import

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { useAuth, ClerkProvider } from "@clerk/nextjs";
 const client = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
